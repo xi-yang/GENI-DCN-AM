4c78b2b18777b15cf0d101bac809aad9a8364396
jflidr <jflidr@2e773745-ba66-0410-b921-47808ed65ce0>
2009-06-22 15:59:28 +0000

    fault class to prevent override by the wsdl generated stuff
    git-svn-id: file:///var/www/svn/geni-aggregate@10 2e773745-ba66-0410-b921-47808ed65ce0

---
+++ AggregateWS/src/net/geni/aggregate/services/api/AggregateFaultMessageExt.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.geni.aggregate.services.api;
+
+/**
+ *
+ * @author jflidr
+ */
+public class AggregateFaultMessageExt extends AggregateFaultMessage
+{
+
+    public static final int SENDER = 1;
+    public static final int INTERNAL = 2;
+}
