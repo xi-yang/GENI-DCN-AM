89147e5ff96bbda01586cbc2353db58b8f138df3
chris <chris@2e773745-ba66-0410-b921-47808ed65ce0>
2010-05-19 14:03:37 +0000

    fixed a bug
    git-svn-id: file:///var/www/svn/geni-aggregate@141 2e773745-ba66-0410-b921-47808ed65ce0

--- AggregateWS/src/net/geni/aggregate/services/core/AggregateRspecRunner.java
+++ AggregateWS/src/net/geni/aggregate/services/core/AggregateRspecRunner.java
@@ -154,7 +154,7 @@ public class AggregateRspecRunner extends Thread {
         String url = "http://" + rspec.getAggregateName();
         String description = "Rspec:" + rspec.getRspecName() + " on aggregate:" + rspec.getAggregateName();
         AggregateSlice aggrSlice = AggregateState.getAggregateSlices().createSlice(sliceName, url, description,
-                (rspec.getUsers()==null?AggregateState.getPlcPI():rspec.getUsers().get(0)), nodes.split(":"));
+                ((rspec.getUsers()==null || rspec.getUsers().isEmpty())?AggregateState.getPlcPI():rspec.getUsers().get(0)), nodes.split(":"));
         if (aggrSlice != null) {
             aggrSlice.setStatus("CREATED");
             aggrSlice.setType("computeSlice");
