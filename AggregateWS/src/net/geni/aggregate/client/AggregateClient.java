4bf8e7f7f86fc0a56bdfa74b46ad0aeb48b331fa
chris <chris@2e773745-ba66-0410-b921-47808ed65ce0>
2010-06-09 20:26:57 +0000

    api cilent and examples
    git-svn-id: file:///var/www/svn/geni-aggregate@152 2e773745-ba66-0410-b921-47808ed65ce0

--- AggregateWS/src/net/geni/aggregate/client/AggregateClient.java
+++ AggregateWS/src/net/geni/aggregate/client/AggregateClient.java
@@ -76,6 +76,133 @@ public class AggregateClient {
         return listCapsResponse.getListCapabilitiesResponse();
     }
 
+    public ListSlicesResponseType listSlices()
+           throws AggregateFaultMessage, Exception {
+        ListSlices listSlices = new ListSlices();
+        ListSlicesType listSlicesType = new ListSlicesType();
+        listSlices.setListSlices(listSlicesType);
+        ListSlicesResponse listSlicesResponse = this.stub.ListSlices(listSlices);
+        return listSlicesResponse.getListSlicesResponse();
+    }
+
+    public ListNodesResponseType listNodes(String[] capUrns)
+           throws AggregateFaultMessage, Exception {
+        ListNodes listNodes = new ListNodes();
+        ListNodesType listNodesType = new ListNodesType();
+        if (capUrns != null) {
+            ListNodesTypeSequence[] listNodesTypeSeq = new ListNodesTypeSequence[capUrns.length];
+            for (int i = 0; i < capUrns.length; i++) {
+                listNodesTypeSeq[i].setCapabilityURN(capUrns[i]);
+            }
+            listNodesType.setListNodesTypeSequence(listNodesTypeSeq);
+        }
+        listNodes.setListNodes(listNodesType);
+        ListNodesResponse listNodesResponse = this.stub.ListNodes(listNodes);
+        return listNodesResponse.getListNodesResponse();
+    }
+
+
+    public CreateSliceResponseType createSlice(String sliceName, String user,
+            String url, String[] nodes, String descr)
+           throws AggregateFaultMessage, Exception {
+        CreateSlice createSlice = new CreateSlice();
+        CreateSliceType createSliceType = new CreateSliceType();
+        createSliceType.setSliceID(sliceName);
+        createSliceType.setUser(user);
+        createSliceType.setUrl(url);
+        createSliceType.setNode(nodes);
+        createSliceType.setDescription(descr);
+        createSlice.setCreateSlice(createSliceType);
+        CreateSliceResponse createSliceResponse = this.stub.CreateSlice(createSlice);
+        return createSliceResponse.getCreateSliceResponse();
+    }
+
+    // Obsolete
+    public StartSliceResponseType startSlice(String sliceName)
+           throws AggregateFaultMessage, Exception {
+        StartSlice StartSlice = new StartSlice();
+        StartSliceType StartSliceType = new StartSliceType();
+        StartSliceType.setSliceID(sliceName);
+        StartSlice.setStartSlice(StartSliceType);
+        StartSliceResponse StartSliceResponse = this.stub.StartSlice(StartSlice);
+        return StartSliceResponse.getStartSliceResponse();
+    }
+
+    // Obsolete
+    public StopSliceResponseType stopSlice(String sliceName)
+           throws AggregateFaultMessage, Exception {
+        StopSlice StopSlice = new StopSlice();
+        StopSliceType StopSliceType = new StopSliceType();
+        StopSliceType.setSliceID(sliceName);
+        StopSlice.setStopSlice(StopSliceType);
+        StopSliceResponse StopSliceResponse = this.stub.StopSlice(StopSlice);
+        return StopSliceResponse.getStopSliceResponse();
+    }
+
+    public DeleteSliceResponseType deleteSlice(String sliceName)
+           throws AggregateFaultMessage, Exception {
+        DeleteSlice deleteSlice = new DeleteSlice();
+        DeleteSliceType deleteSliceType = new DeleteSliceType();
+        deleteSliceType.setSliceID(sliceName);
+        deleteSlice.setDeleteSlice(deleteSliceType);
+        DeleteSliceResponse deleteSliceResponse = this.stub.DeleteSlice(deleteSlice);
+        return deleteSliceResponse.getDeleteSliceResponse();
+    }
+
+    public QuerySliceResponseType querySlice(String[] sliceNames)
+           throws AggregateFaultMessage, Exception {
+        QuerySlice querySlice = new QuerySlice();
+        QuerySliceType querySliceType = new QuerySliceType();
+        querySliceType.setSliceID(sliceNames);
+        querySlice.setQuerySlice(querySliceType);
+        QuerySliceResponse querySliceResponse = this.stub.QuerySlice(querySlice);
+        return querySliceResponse.getQuerySliceResponse();
+    }
+
+    public CreateSliceVlanResponseType createSliceVlan(String sliceName, String vlan,
+            float bw, String srcNode, String srcInterface, String srcIP,
+            String dstNode, String dstInterface, String dstIP)
+           throws AggregateFaultMessage, Exception {
+        CreateSliceVlan createSliceVlan = new CreateSliceVlan();
+        CreateSliceVlanType createSliceVlanType = new CreateSliceVlanType();
+        createSliceVlanType.setSliceID(sliceName);
+        VlanReservationDescriptorType vlanResvDescr = new VlanReservationDescriptorType();
+        vlanResvDescr.setVlan(vlan);
+        vlanResvDescr.setBandwidth(bw);
+        vlanResvDescr.setSourceNode(srcNode);
+        vlanResvDescr.setSrcInterface(srcInterface);
+        vlanResvDescr.setSrcIpAndMask(srcIP);
+        vlanResvDescr.setDestinationNode(dstNode);
+        vlanResvDescr.setDstInterface(dstInterface);
+        vlanResvDescr.setDstIpAndMask(dstIP);
+        createSliceVlanType.setVlanReservation(vlanResvDescr);
+        createSliceVlan.setCreateSliceVlan(createSliceVlanType);
+        CreateSliceVlanResponse createSliceVlanResponse = this.stub.CreateSliceVlan(createSliceVlan);
+        return createSliceVlanResponse.getCreateSliceVlanResponse();
+    }
+
+    public DeleteSliceVlanResponseType deleteSliceVlan(String sliceName, String vlan)
+           throws AggregateFaultMessage, Exception {
+        DeleteSliceVlan deleteSliceVlan = new DeleteSliceVlan();
+        DeleteSliceVlanType deleteSliceVlanType = new DeleteSliceVlanType();
+        deleteSliceVlanType.setSliceID(sliceName);
+        deleteSliceVlanType.setVlan(vlan);
+        deleteSliceVlan.setDeleteSliceVlan(deleteSliceVlanType);
+        DeleteSliceVlanResponse deleteSliceVlanResponse = this.stub.DeleteSliceVlan(deleteSliceVlan);
+        return deleteSliceVlanResponse.getDeleteSliceVlanResponse();
+    }
+
+    public QuerySliceVlanResponseType querySliceVlan(String sliceName, String vlan)
+           throws AggregateFaultMessage, Exception {
+        QuerySliceVlan querySliceVlan = new QuerySliceVlan();
+        QuerySliceVlanType querySliceVlanType = new QuerySliceVlanType();
+        querySliceVlanType.setSliceID(sliceName);
+        querySliceVlanType.setVlan(vlan);
+        querySliceVlan.setQuerySliceVlan(querySliceVlanType);
+        QuerySliceVlanResponse querySliceVlanResponse = this.stub.QuerySliceVlan(querySliceVlan);
+        return querySliceVlanResponse.getQuerySliceVlanResponse();
+    }
+
     public CreateSliceNetworkResponseType createSliceNetwork(String rsepcXml[])
            throws AggregateFaultMessage, Exception {
         CreateSliceNetwork createSliceNet = new CreateSliceNetwork();
---
+++ AggregateWS/src/net/geni/aggregate/client/examples/ListNodesClient.java
@@ -0,0 +1,60 @@
+package net.geni.aggregate.client.examples;
+
+import java.io.*;
+
+import net.geni.aggregate.client.*;
+import net.geni.aggregate.client.AggregateGENIStub.*;
+/**
+ *
+ * @author Xi Yang
+ */
+public class ListNodesClient extends ExampleClient {
+    public static void main(String[] args) {
+        try {
+            ListNodesClient cl = new ListNodesClient();
+            cl.listNodes(args);
+        } catch (AggregateFaultMessage e) {
+            System.out.println(
+                    "AggregateFaultMessage from ListNodesClient");
+            System.out.println(e.getFaultMessage().getMsg());
+        } catch (Exception e) {
+            System.out.println("AggregateGENIStub threw exception");
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public void listNodes(String[] args)
+            throws AggregateFaultMessage, Exception {
+
+        super.init(args);
+
+        String capUrn = "";
+        try {
+            // Prompt for input parameters
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            capUrn = Args.getArg(br, "Rspec ID/Name", capUrn);
+            br.close();
+        } catch (IOException ioe) {
+            System.out.println("IO error reading input");
+            System.exit(1);
+        }
+        // make the call to the server
+        String[] capUrns = {capUrn};
+        ListNodesResponseType response = this.getClient().listNodes(capUrn.isEmpty() ? null : capUrns);
+        this.outputResponse(response);
+        super.cleanup();
+    }
+
+    public void outputResponse(ListNodesResponseType response) {
+        System.out.println("============ ListNodes Response =========== ");
+        ListNodesResponseTypeSequence[] listNodesTypeSeq = response.getListNodesResponseTypeSequence();
+        for (ListNodesResponseTypeSequence listNodesType: listNodesTypeSeq) {
+            NodeDescriptorType node = listNodesType.getNode();
+            System.out.println("Node: " + Integer.toString(node.getId()));
+            System.out.println("\t URN => " + node.getUrn());
+            System.out.println("\t Description => " + node.getDescription());
+            System.out.println();
+        }
+    }
+}
