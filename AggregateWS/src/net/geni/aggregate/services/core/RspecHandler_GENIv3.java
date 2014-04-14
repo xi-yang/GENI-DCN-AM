6e4551ca10fa61ef718ecb4f595fad506c4e848b
xi <xi@2e773745-ba66-0410-b921-47808ed65ce0>
2014-04-14 14:55:04 +0000

    bug fix
    git-svn-id: file:///var/www/svn/geni-aggregate/trunk@501 2e773745-ba66-0410-b921-47808ed65ce0

--- AggregateWS/src/net/geni/aggregate/services/core/RspecHandler_GENIv3.java
+++ AggregateWS/src/net/geni/aggregate/services/core/RspecHandler_GENIv3.java
@@ -534,9 +534,10 @@ public class RspecHandler_GENIv3 implements AggregateRspecHandler {
     }
 
     public String generateAdvertisementRspec(AggregateRspec rspec) throws AggregateException {
-        Date dateExpires = new Date();
+        Date dateNow = new Date();
         GregorianCalendar c = new GregorianCalendar();
-        c.setTime(dateExpires);
+        c.setTimeInMillis(dateNow.getTime());
+        c.add(Calendar.DAY_OF_MONTH, 60);
         XMLGregorianCalendar xgcExpires = null;
         try {
             xgcExpires = DatatypeFactory.newInstance().newXMLGregorianCalendar(c);
