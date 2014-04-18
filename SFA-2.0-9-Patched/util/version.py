eff6c516f85961a15e196d8cea0333081c5de519
xi <xi@2e773745-ba66-0410-b921-47808ed65ce0>
2014-04-18 20:30:20 +0000

    fixed code_tag and code_url
    git-svn-id: file:///var/www/svn/geni-aggregate/branches/r2.0@504 2e773745-ba66-0410-b921-47808ed65ce0

--- SFA-2.0-9-Patched/util/version.py
+++ SFA-2.0-9-Patched/util/version.py
@@ -1,11 +1,15 @@
 ### updated by the toplevel Makefile
 version_tag="2.0-9"
 scm_url="git://git.onelab.eu/sfa.git@sfa-2.0-9"
+svn_info="$Revision: 503 $ $HeadURL: https://svn.maxgigapop.net/svn/geni-aggregate/branches/r2.0 $"
 import socket
+import re
+ 
+m = re.search('Revision: ([^ ]+) \$ \$HeadURL: ([^ ]+) ', svn_info)
  
 def version_core (more={}):
-    core = { 'code_tag' : version_tag,
-             'code_url' : scm_url,
+    core = { 'code_tag' : 'r2.0-'+m.group(1),
+             'code_url' : m.group(2),
              'hostname' : socket.gethostname(),
              }
     core.update(more)
