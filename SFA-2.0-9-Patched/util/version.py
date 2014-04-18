bc4ca6e6757243bde5fd0a1cf651294dd5ce7e27
xi <xi@2e773745-ba66-0410-b921-47808ed65ce0>
2014-04-18 20:31:23 +0000

    svn:keywords Revision HeadURL
    git-svn-id: file:///var/www/svn/geni-aggregate/branches/r2.0@505 2e773745-ba66-0410-b921-47808ed65ce0

--- SFA-2.0-9-Patched/util/version.py
+++ SFA-2.0-9-Patched/util/version.py
@@ -1,7 +1,7 @@
 ### updated by the toplevel Makefile
 version_tag="2.0-9"
 scm_url="git://git.onelab.eu/sfa.git@sfa-2.0-9"
-svn_info="$Revision: 503 $ $HeadURL: https://svn.maxgigapop.net/svn/geni-aggregate/branches/r2.0 $"
+svn_info="$Revision$ $HeadURL$"
 import socket
 import re
  
